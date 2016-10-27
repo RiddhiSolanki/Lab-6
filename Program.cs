6d3b92343b33d27fc09524e39272d7e8c55c8b80
RiddhiSolanki <riddhisolanki91@gmail.com>
2016-10-27 19:58:04 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using Lab_6;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PigLatinTranslator
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            string keepGoing = "y";
+            do
+            {
+                Console.WriteLine("Welcome to the Pig latin Translator.Enter a line to be translated");
+                string ans = Console.ReadLine();
+
+                Translator brad = new Translator();
+                string sword = brad.Translate(ans);
+                Console.WriteLine(sword);
+
+                    Console.WriteLine("continue y/n");
+                    keepGoing = Console.ReadLine();
+            }
+
+            while (keepGoing=="y");
+            
+
+          
+        }
+    }
+}
---
+++ Translator.cs
@@ -0,0 +1,42 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_6
+{
+    public class Translator
+    {
+        public string Substring { get; private set; }
+
+        public string Translate(string wordToTranslate)
+        {
+
+            wordToTranslate = wordToTranslate.ToLower();
+            string firstLetter = "";
+            if (wordToTranslate.StartsWith("a") || wordToTranslate.StartsWith("e") || wordToTranslate.StartsWith("i") || wordToTranslate.StartsWith("o") || wordToTranslate.StartsWith("u"))
+
+            {
+                firstLetter = wordToTranslate.Substring(0, 1);
+                return wordToTranslate + "way";
+            }
+            else
+            {
+                int indexoffirstvowel = 1;
+                string Letteratindex = wordToTranslate.Substring(indexoffirstvowel, 1);
+
+
+                while
+                    (!(Letteratindex == "a" || Letteratindex == "e" || Letteratindex == "i" || Letteratindex == "o" || Letteratindex == "u"))
+                  {
+                    indexoffirstvowel++;
+                    Letteratindex = wordToTranslate.Substring(indexoffirstvowel, 1);
+                }
+
+                return (wordToTranslate.Substring(indexoffirstvowel) + wordToTranslate.Substring(0, indexoffirstvowel) + "ay");
+
+            }
+        }
+    }
+}
